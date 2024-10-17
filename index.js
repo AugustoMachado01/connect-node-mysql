a24a71728b043e0f90a3a9ab01f4a44e7e7d745e
AugustoMachado01 <augustomachadochieno01@gmail.com>
2024-10-17 10:49:43 -0400

    Delete data

--- index.js
+++ index.js
@@ -67,6 +67,54 @@ app.get("/books/:id", (req, res) => {
   });
 });
 
+app.get("/books/edit/:id", (req, res) => {
+  const id = req.params.id;
+
+  const sql = `SELECT * FROM books WHERE id = ${id}`;
+
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const book = data[0];
+
+    res.render("editbook", { book });
+  });
+});
+
+app.post("/books/updatebook", (req, res) => {
+  const id = req.body.id;
+  const title = req.body.title;
+  const pageqty = req.body.pageqty;
+  const sql = `UPDATE books SET title = '${title}', pageqty = ${pageqty} WHERE id = ${id}`;
+
+  conn.query(sql, function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  });
+
+  res.redirect("/books");
+});
+
+app.post("/books/remove/:id", (req, res) => {
+  const id = req.params.id;
+
+  sql = `DELETE FROM books WHERE id = ${id}`;
+
+  conn.query(sql, function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  });
+
+  res.redirect("/books");
+});
+
 const conn = mysql.createConnection({
   host: "localhost",
   user: "root",
